6ab934e51ceb0fa5e1459a0e7aaf50164585e511
Andreea Macoveiciuc <andreea.macoveiciuc@gmail.com>
2020-04-12 20:26:10 +0200

    Updates

---
+++ precache-manifest.8502cdf94ab31ba1882a3e2a3ca29ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e825f2e5f480fa209c34395dbba74d",
+    "url": "/react-storybook-tutorial/index.html"
+  },
+  {
+    "revision": "56349d02e493173edfaf",
+    "url": "/react-storybook-tutorial/static/css/main.45b2bdba.chunk.css"
+  },
+  {
+    "revision": "fdab32022b7e6a935a3f",
+    "url": "/react-storybook-tutorial/static/js/2.8625fa98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-storybook-tutorial/static/js/2.8625fa98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56349d02e493173edfaf",
+    "url": "/react-storybook-tutorial/static/js/main.de730df1.chunk.js"
+  },
+  {
+    "revision": "9724ac2e63f589375e6c",
+    "url": "/react-storybook-tutorial/static/js/runtime-main.155764ed.js"
+  }
+]);
